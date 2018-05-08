d4d1fcde27cd7ee31f31b2fd863f9b7944e384a0
Gabriel Souza <gterhorsts@gmail.com>
2018-05-08 11:07:51 -0300

    Correção do Bug

--- jogo.py
+++ jogo.py
@@ -1,83 +1,91 @@
 #!/usr/bin/env python
 import pygame
-import animacao_personagem
+import personagem
 
 pygame.init()
 
-comprimento_display = 800
-altura_display = 600
 
 texto_grande = pygame.font.Font('freesansbold.ttf', 115)
 texto_pequeno = pygame.font.Font('freesansbold.ttf', 30)
 
+tela = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Python/Pygame Animation")
+relogio = pygame.time.Clock()
+player = personagem.Neguinho((150, 150))
+
+comprimento_display = 800
+altura_display = 600
+
 preto = (0,0,0)
 branco = (255,255,255)
 
-#cria tela
-tela = pygame.display.set_mode((comprimento_display,altura_display))
-pygame.display.set_caption('nome do jogo')
-relogio = pygame.time.Clock()
-player = animacao_personagem.Serge((150, 150))
+
 
 def texto(texto, fonte):
     tipo_texto = fonte.render(texto, True, preto)
     return tipo_texto, tipo_texto.get_rect()
 
-def butao(mensagem, x, y, largura, altura, cor_inativa, cor_ativa, acao = None):
+def butao(mensagem, x, y, largura, altura, cor_inativa, cor_ativa):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+
     if x + largura > mouse[0] > x and y + altura > mouse[1] > y:
         pygame.draw.rect(tela, cor_ativa,(x,y,largura,altura))
         if click[0] == 1:
-            acao()
+            return True
     else:
         pygame.draw.rect(tela,cor_inativa,(x,y,largura,altura))
+        
     texto_botao = pygame.font.Font('freesansbold.ttf', 20)
     textSurf, textRect = texto(mensagem, texto_botao)
     textRect.center = ((x+(largura/2)), (y+(altura/2)))
     tela.blit(textSurf, textRect)
+    return False
 
 def menu():
-    intro = True
-    while intro:
+    while True:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                return -1
         tela.fill(branco)
         TextSurf, TextRect = texto('nome do jogo', texto_grande)
         TextRect.center = ((comprimento_display/2), (altura_display/2))
         tela.blit(TextSurf, TextRect)
 
-        butao('jogar', 150,450,100,50,(0,255,0),(0,200,0), gameloop)
-        butao('sair', 550,450,100,50,(255,0,0),(200,0,0))
+        if butao('jogar', 150,450,100,50,(0,255,0),(0,200,0)):
+            return 1
+        if butao('sair', 550,450,100,50,(255,0,0),(200,0,0)):
+            return -1
 
         pygame.display.update()
         relogio.tick(15)
 
+def jogo():
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                return -1
+            player.handle_event(evento)                 
+        tela.fill(pygame.Color('blue'))  
+        tela.blit(player.image, player.rect)
+        
+        pygame.display.flip()              
+        relogio.tick(10)    
 
 def gameloop():
-
-
-    
+    # Variavel estado: em que estado o jogo se encontra
+    #
+    # -1: game over
+    # 0: tela inicial
+    # 1: proxima tela
+    estado = 0
     #enquanto o jogo esta aberto
-    fora_do_jogo = False
-    while not fora_do_jogo:
-
-	for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-            	    fora_do_jogo = True
-    	player.handle_event(event)
-    	tela.fill(pygame.Color('blue'))
-    	tela.blit(player.image, player.rect)
-    	pygame.display.flip()
-
-        menu()
-        
-        pygame.display.flip()
-        #FPS
-        relogio.tick(60)
-
+    while estado != -1:
+        if estado == 0:
+            estado = menu()
+        elif estado == 1:
+            print('Funcionando')
+            estado = jogo()
+ 
 gameloop()
 pygame.quit()
-quit()
