42bb1386edecd98327b78397137a334d032a4393
Gabriel Souza <gterhorsts@gmail.com>
2018-05-16 22:35:43 -0300

    Mudança no codigo da animação para comportar a interação(ainda não funcional)

--- animacao_personagem.py
+++ animacao_personagem.py
@@ -1,9 +1,10 @@
 #Esse código foi copiado e adaptado do seguinte site: http://xorobabel.blogspot.com.br/2012/10/pythonpygame-2d-animation-jrpg-style.html
 
-
 import pygame
+import interação_objetos as io
  
-class Serge(pygame.sprite.Sprite):
+class Neguinho(pygame.sprite.Sprite):
+    
     def __init__(self, position):
         self.sheet = pygame.image.load('personagem.png')
         self.sheet.set_clip(pygame.Rect(0, 0, 52, 76))
@@ -13,8 +14,9 @@ class Serge(pygame.sprite.Sprite):
         self.frame = 0
         self.left_states = { 0: (0, 76, 52, 76), 1: (52, 76, 52, 76), 2: (156, 76, 52, 76) }
         self.right_states = { 0: (0, 152, 52, 76), 1: (52, 152, 52, 76), 2: (156, 152, 52, 76) }
+        self.direction = 'stand_left'
     
- 
+    
     def get_frame(self, frame_set):
         self.frame += 1
         if self.frame > (len(frame_set) - 1):
@@ -28,22 +30,6 @@ class Serge(pygame.sprite.Sprite):
             self.sheet.set_clip(pygame.Rect(clipped_rect))
         return clipped_rect
        
-    def update(self, direction):
-        if direction == 'left':
-            self.clip(self.left_states)
-            self.rect.x -= 5
-        if direction == 'right':
-            self.clip(self.right_states)
-            self.rect.x += 5
- 
-        if direction == 'stand_left':
-            self.clip(self.left_states[0])
-        if direction == 'stand_right':
-            self.clip(self.right_states[0])
-    
- 
-        self.image = self.sheet.subsurface(self.sheet.get_clip())
- 
     def update(self):
         if self.direction == 'left':
             self.clip(self.left_states)
@@ -51,25 +37,37 @@ class Serge(pygame.sprite.Sprite):
         if self.direction == 'right':
             self.clip(self.right_states)
             self.rect.x += 5
- 
+        if self.direction == "cima":
+            self.clip(self.right_states)
+            io.Teste
+            
         if self.direction == 'stand_left':
             self.clip(self.left_states[0])
         if self.direction == 'stand_right':
             self.clip(self.right_states[0])
     
         self.image = self.sheet.subsurface(self.sheet.get_clip())
- 
+
+
+
     def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-           
+        if event.type == pygame.KEYDOWN:           
             if event.key == pygame.K_LEFT:
                 self.direction = 'left'
             if event.key == pygame.K_RIGHT:
                 self.direction = 'right'
- 
+            
+            if event.key == pygame.K_UP:
+                if player.rect.x >= 728 and player.rect.x < 736:
+                    self.direction = "cima"
+                    
         if event.type == pygame.KEYUP:  
  
             if event.key == pygame.K_LEFT:
                 self.direction = 'stand_left'           
             if event.key == pygame.K_RIGHT:
                 self.direction = 'stand_right'
+                
+           
+player = Neguinho((0, 475))
+
